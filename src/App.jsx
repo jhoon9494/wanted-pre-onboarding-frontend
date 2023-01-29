57e5cac7acc3237cc8f73f65febcd95b77aef8b9
jhoon9494 <wjdgns9406@gmail.com>
2023-01-29 17:08:00 +0900

    Feat: Todolist 페이지 추가 #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Todo from './pages/Todo';
 import Globalstyle from './style/GlobalStyle';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path={'/'} element={<SignIn />} />
         <Route path={'/signup'} element={<SignUp />} />
         <Route path={'/signin'} element={<SignIn />} />
+        <Route path={'/todo'} element={<Todo />} />
       </Routes>
     </>
   );
