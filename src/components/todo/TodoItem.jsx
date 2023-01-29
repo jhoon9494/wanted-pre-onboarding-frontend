a57bffa4ed58b4536a02d37275bf649b6a3e1ccd
jhoon9494 <wjdgns9406@gmail.com>
2023-01-29 14:33:14 +0900

    Feat: 체크박스로 완료 여부확인 추가 #7

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import API from '../../api';
 
 // Styled-Components
 const List = styled.li`
@@ -12,10 +14,32 @@ const List = styled.li`
 
 // TodoItem Components
 const TodoItem = ({ todo }) => {
+  const [isCompleted, setIsCompleted] = useState(todo.isCompleted);
+  const handleCheck = async () => {
+    try {
+      await API.put(
+        `todos/${todo.id}`,
+        {
+          todo: todo.todo,
+          isCompleted: !isCompleted,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+        }
+      );
+      setIsCompleted((curr) => !curr);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // console.log(todo);
   return (
     <List>
       <label>
-        <input type="checkbox" /> <span>{todo.todo}</span>
+        <input type="checkbox" checked={isCompleted} onChange={handleCheck} /> <span>{todo.todo}</span>
       </label>
       <button data-testid="modify-button">수정</button>
       <button data-testid="delete-button">삭제</button>
